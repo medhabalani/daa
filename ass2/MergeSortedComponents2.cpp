eb232a221d0da6759d650ada1a4e3b21c4314846
saksham701 <56355498+saksham701@users.noreply.github.com>
2021-02-05 13:36:56 +0530

    Update and rename Approach2(Set).cpp to MergeSortedComponents2.cpp

--- ass2/MergeSortedComponents2.cpp
+++ ass2/MergeSortedComponents2.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 int main() {
 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
 
     int n;  //size of input array
     cin >> n;
